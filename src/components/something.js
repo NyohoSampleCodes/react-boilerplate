200a0fbb1bf3526d5909285503ec49fe47dfd069
北䑓如法 <algebraicallyClosedField@gmail.com>
2018-03-21 18:54:40 +0900

    fix by lint

--- src/components/something.js
+++ src/components/something.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 export default class Something extends React.Component {
   render() {
@@ -6,6 +6,6 @@ export default class Something extends React.Component {
       <div>
         <p>Something special</p>
       </div>
-    );
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Something from './components/something.js';
-import './main.scss';
-import './sub.sass';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Something from './components/something.js'
+import './main.scss'
+import './sub.sass'
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        Hello, {this.props.name}!
+        Hello, {name}!
         <Something />
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(
-  <Main name='Thom' />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Main name="Thom" />, document.getElementById('root'))
