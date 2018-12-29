c1df2ee668a6c0f891af2626e2363a630702520c
北䑓如法 <algebraicallyClosedField@gmail.com>
2018-12-29 10:44:01 +0900

    prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,37 @@
-const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 const pkg = require('./package')
 const DEV_PORT = process.env.PORT || 4444
 
 module.exports = {
   mode: 'development',
-  
+
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.txt$/,
-        use: 'raw-laoder'
+        use: 'raw-laoder',
       },
       {
         test: /\.(sass|scss)$/,
-        use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: ['css-loader', 'sass-loader']
-        })
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
-    ]
+    ],
   },
   devtool: 'source-map',
-  plugins: [
-    new ExtractTextPlugin("style.css"),
-  ],
+  plugins: [new MiniCssExtractPlugin({ filename: 'style.css' })],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
-};
+}
